9b075cd0b26d5dd145f64e9ef40ec4115f6e024c
Diogo Alves <diogomoura2014@gmail.com>
2024-06-03 15:28:48 +0100

    olá pinto

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pyone
-
-
-one = pyone.OneServer("http://34.22.235.0:2633/RPC2", session="oneadmin:12345")
+import tkinter
+one = pyone.OneServer("http://34.38.189.124:2633/RPC2", session="oneadmin:12345")
 hostpool = one.hostpool.info()
 host = hostpool.HOST[0]
-print()
+id = host.ID
+m = tkinter.Tk()
+m.title('OpenNebula GUI')
+m.geometry('1080x960')
+m.configure(background='navy')
+m.mainloop()
