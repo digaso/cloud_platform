842a3ab456236fb5fd336edb271847e02a437cd9
Kingarooo <guipinto12@gmail.com>
2024-06-06 18:24:06 +0100

    Image allocation, ubunto image added e inicio de criação de VM

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import pyone
 
 
-one = pyone.OneServer("http://34.22.235.0:2633/RPC2", session="oneadmin:12345")
+one = pyone.OneServer("http://34.38.189.124:2633/RPC2", session="oneadmin:12345")
 hostpool = one.hostpool.info()
 host = hostpool.HOST[0]
-print()
+print("Host information:")
 # Step 1: Create a Disk Image
 image_template = '''
-NAME="my_disk_image"
-PATH="/var/lib/one/datastores/0/disk.img"  # Adjust this path as necessary
+NAME="Disk_Test"
+PATH="./ubuntu_20_04.qcow2"  
 TYPE="OS"
 PERSISTENT="YES"
 '''
 
+# images = one.imagepool.info(-2, -1, -1)
+# for image in images.IMAGE:
+#     print(f"ID: {image.ID}, Name: {image.NAME}, Type: {image.TYPE}, Persistent: {image.PERSISTENT}")
+
+
 # Allocate the image
-datastore_id = 0  # Adjust this as needed
+datastore_id = 1  # Adjust this as needed
 image_id = one.image.allocate(image_template, datastore_id)
 print(f"Image ID: {image_id}")
+
 
 one.template.allocate('''
   NAME="test100"
   MEMORY="1024"
-  DISK=[ ${image_id} ]
+  DISK=[ {image_id} ]
   CPU="1"
   VCPU="2"
 ''')
+print("Template ID: {template_id}")
 
