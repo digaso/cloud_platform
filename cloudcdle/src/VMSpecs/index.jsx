5fcf7d02cfeb73babd6fb60d4e343c706dfd14f3
Kingarooo <guipinto12@gmail.com>
2024-06-09 07:27:56 +0100

    Yoyoyoyo

--- cloudcdle/src/VMSpecs/index.jsx
+++ cloudcdle/src/VMSpecs/index.jsx
@@ -5,38 +5,27 @@ import { useNavigate } from 'react-router-dom';
 const VMSpecs = () => {
   const { vmId } = useParams();
   const [vmData, setVmData] = useState(null);
+  const [vmlist, setVmlist] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
   const navigate = useNavigate();
   const username = localStorage.getItem('username');
   const handleNavigateToMyAccount = () => {
       navigate("/my-account");
       }
       useEffect(() => {
-          axios.get('http://localhost:8080/user/vm_list', username)
+          axios.get('http://localhost:8080/user/vm_list', {params:{username}})
           .then((response) => {
-              console.log(response.data);
-        const vm = response.data;
-        const vmSpecs = {
-          ID: vm.ID,
-          Name: vm.NAME,
-          State: vm.STATE,
-          Owner: vm.UNAME,
-          UID: vm.UID,
-          GID: vm.GID,
-          Memory: vm.MEMORY,
-          CPU: vm.CPU,
-          OS: vm.OS,
-          Disk: vm.DISK,
-          Context: vm.CONTEXT
-        };
-        setVmData(vmSpecs);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError(error);
-        setLoading(false);
-      });
+                setVmlist(response.data);
+                const data = response.data;
+                setLoading(false);
+                console.log(vmlist);
+                })
+                .catch((error) => {
+                    setError(error);
+                    setLoading(false);
+                    });
   }, [vmId]);
 
   if (loading) {
@@ -52,21 +41,16 @@ const VMSpecs = () => {
         <button onClick={handleNavigateToMyAccount} style={styles.button}>
                 Back
             </button>
-      <h1>VM Specifications</h1>
+      <h1>VM List</h1>
       {vmData && (
           <div style={styles.vmData}>
-          <p><strong>ID:</strong> {vmData.ID}</p>
-          <p><strong>Name:</strong> {vmData.Name}</p>
-          <p><strong>State:</strong> {vmData.State}</p>
-          <p><strong>Owner:</strong> {vmData.Owner}</p>
-          <p><strong>UID:</strong> {vmData.UID}</p>
-          <p><strong>GID:</strong> {vmData.GID}</p>
-          <p><strong>Memory:</strong> {vmData.Memory}</p>
-          <p><strong>CPU:</strong> {vmData.CPU}</p>
-          <p><strong>OS:</strong> {vmData.OS}</p>
-          <p><strong>Disk:</strong> {JSON.stringify(vmData.Disk)}</p>
-          <p><strong>Context:</strong> {JSON.stringify(vmData.Context)}</p>
-        </div>
+              {vmlist.map((vm, index) => (
+                  <div key={index}>
+                      <h2>{}</h2>
+                      <p>Size: {vm.Disk.SIZE}</p>
+                  </div>
+              ))}
+          </div>
       )}
     </div>
   );
